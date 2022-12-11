12ca95d7693ab5af1dfb5f2630dc71cc384c3329
jrubialesv <jrubialesv@gmail.com>
2022-12-11 13:49:05 +0100

    Test after new cred

--- recipes_api/__init__.py
+++ recipes_api/__init__.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 load_dotenv()
-# Select environment based on the ENV environment variable
+# Select environment based on the ENV environment variabled
 
 
 if os.getenv('ENV') != 'dev':
